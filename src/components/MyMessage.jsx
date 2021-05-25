08c5346b05c42e085a8dc563f2b4daf7b15fb0e9
ulrichschulzendorf <ulrich.s.schulzendorf@gmail.com>
2021-05-25 18:28:58 +0200

    mymessage done 31.54

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,5 +1,25 @@
-const MyMessage = () => {
-  <div>MyMessage</div>;
+const MyMessage = ({ message }) => {
+  if (message?.attachements?.lenght > 0) {
+    return (
+      <img
+        src={message.attachements[0].file}
+        alt="message-attachment"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+  <div
+    className="message"
+    style={{
+      float: "right",
+      marginRight: "18px",
+      color: "white",
+      backgroundColor: "#336699",
+    }}
+  >
+    {message.text}
+  </div>;
 };
 
 export default MyMessage;
