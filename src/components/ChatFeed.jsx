4a0a8c53d2480407d229a1155a709d5eb46a6148
ulrichschulzendorf <ulrich.s.schulzendorf@gmail.com>
2021-05-25 18:12:11 +0200

    chat feed done 29.27

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -16,7 +16,14 @@ const ChatFeed = (props) => {
       return (
         <div key={`mag_${index}`} style={{ width: "100%" }}>
           <div className="message-block">
-            {isMyMessage ? <MyMessage /> : <TheirMessage />}
+            {isMyMessage ? (
+              <MyMessage message={message} />
+            ) : (
+              <TheirMessage
+                message={message}
+                lastMessage={messages[lastMessageKey]}
+              />
+            )}
           </div>
           <div
             className="read-receipts"
